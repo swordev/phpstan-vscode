f2453e2758a3ca6e76d27423f9f7ed1c28285502
Juanra GM <juanrgm724@gmail.com>
2022-09-22 00:33:20 +0200

    chore: build the extension using the next version

--- scripts/build-vsce.mjs
+++ scripts/build-vsce.mjs
@@ -1,6 +1,7 @@
+import getReleasePlan from "@changesets/get-release-plan";
 import { spawnSync } from "child_process";
 import { readFile, writeFile } from "fs/promises";
-import { dirname } from "path";
+import { dirname, join } from "path";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -8,24 +9,52 @@ const rootPath = dirname(dirname(__filename));
 const vsc = `${rootPath}/node_modules/vsce/vsce`;
 const pkgPath = `${rootPath}/package.json`;
 const pkgContents = await readFile(pkgPath);
-const pkg = JSON.parse(pkgContents);
-const pkgName = pkg.name;
+const pkg = JSON.parse(pkgContents.toString());
+
+async function getBranchName() {
+  const contents = (await readFile(join(rootPath, ".git/HEAD"))).toString();
+  const parts = contents.trim().split("/");
+  return parts[parts.length - 1];
+}
+
+async function getCommitId(branch) {
+  if (!branch) branch = await getBranchName();
+  const contents = await readFile(join(rootPath, ".git/refs/heads", branch));
+  return contents.toString().trim();
+}
+
+async function getNextVersion() {
+  const result = await getReleasePlan.default(rootPath);
+  const newVersion = result.releases.find(
+    (release) => release.name === pkg.name
+  )?.newVersion;
+  return newVersion
+    ? `${newVersion}-${(await getCommitId()).slice(0, 8)}`
+    : pkg.version;
+}
+
+async function writePackageFile(pkg) {
+  await writeFile(pkgPath, JSON.stringify(pkg, null, 2) + "\n");
+}
 
 try {
-  pkg.name = "phpstan";
-  await writePackageFile(pkg);
+  const newPkg = { ...pkg };
+  newPkg.name = "phpstan";
+  newPkg.version = await getNextVersion();
+  await writePackageFile(newPkg);
   spawnSync(
     "node",
-    [vsc, "package", "--no-dependencies", "--out", "swordev.phpstan.vsix"],
+    [
+      vsc,
+      "package",
+      "--no-dependencies",
+      "--out",
+      `swordev.phpstan-${newPkg.version}.vsix`,
+    ],
     {
       stdio: "inherit",
     }
   );
 } finally {
-  pkg.name = pkgName;
   await writePackageFile(pkg);
-}
-
-async function writePackageFile(pkg) {
-  await writeFile(pkgPath, JSON.stringify(pkg, null, 2) + "\n");
 }
